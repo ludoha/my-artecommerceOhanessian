cdab7abc4cb0cc3ac45ce14f5ad5f66e53caa74a
ludoha <ludmilaohanessian@hotmail.com>
2022-07-18 20:21:22 -0300

    contador con boton

--- src/components/ItemListContainer/index.jsx
+++ src/components/ItemListContainer/index.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
 import Title from '../Title';
+import ItemCount from '../ItemCount';
 
 export const ItemListContainer = ({ texto }) => {
+
+    const onAdd = (quantify) => {
+    console.log(`Compraste ${quantify} unidades`);
+    }
     return (
+        <>
         <Title greeting={texto} />
+        <ItemCount initial={1} stock={5} onAdd={onAdd} />
+        </>
     );
 }
 
